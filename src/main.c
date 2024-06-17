ae4566650dfe567120575c5094aa1319a2907632
Neolux Lee <neolux_lee@outlook.com>
2024-06-18 01:09:09 +0800

    Init program

---
+++ src/main.c
@@ -0,0 +1,7 @@
+#include "main.h"
+
+int main(int argc, char const *argv[])
+{
+    
+    return 0;
+}
---
+++ src/main.h
@@ -0,0 +1,12 @@
+#ifndef __MAIN_H__
+#define __MAIN_H__
+
+#include "stdio.h"
+#include "stdlib.h"
+#include "stdint.h"
+#include "stdbool.h"
+#include "string.h"
+#include "math.h"
+#include "time.h"
+
+#endif
